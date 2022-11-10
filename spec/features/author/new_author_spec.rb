3c58a16be6fca01cde150607120790ec3d124db3
Franka-T <frankatraupe@hotmail.com>
2022-11-10 13:45:23 +0100

    #4 add failing test

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -12,4 +12,14 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "should save a author" do
+    visit new_author_path
+    page.fill_in 'author[first_name]', with: 'Edsger'
+    page.fill_in 'author[last_name]', with: 'Dijkstra'
+    page.fill_in 'author[homepage]', with: 'https://de.wikipedia.org/wiki/Edsger_W._Dijkstra'
+    find('input[type="submit"]').click
+    #expect(Author.where(first_name: "Edsger").first.homepage).to eq("https://de.wikipedia.org/wiki/Edsger_W._Dijkstra")
+    #expect(Author.where(first_name: "AAA".size)).to eq(0)
+  end
 end
