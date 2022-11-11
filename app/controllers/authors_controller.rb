bbdf5b3b55463a592882a73fa63412f0e6cdd0fa
Franka-T <frankatraupe@hotmail.com>
2022-11-11 19:33:14 +0100

    #8 fix test

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,6 +3,10 @@ class AuthorsController < ApplicationController
         @author = Author.new
     end
 
+    def edit 
+        @author = Author.find(params[:id])
+    end
+
     def create
         @author = Author.new(author_params)
         #@author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
@@ -12,6 +16,16 @@ class AuthorsController < ApplicationController
         else 
             render 'new'
         end
+    end
+
+    def update 
+        @author = Author.find(params[:id])
+
+        if @author.update(author_params)
+            redirect_to @author
+        else
+            render 'edit'
+        end 
     end
 
     def index
